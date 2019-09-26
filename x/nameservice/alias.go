e0a49130b14e5b228237494b236d18e114b10551
Ijonas Kisselbach <ijonas@ijonas.com>
2019-09-26 07:03:40 +0100

    Initial import

---
+++ x/nameservice/alias.go
@@ -0,0 +1,29 @@
+package nameservice
+
+import (
+	"github.com/ijonas/nameservice/x/nameservice/types"
+)
+
+const (
+	ModuleName = types.ModuleName
+	RouterKey  = types.RouterKey
+	StoreKey   = types.StoreKey
+)
+
+var (
+	NewMsgBuyName    = types.NewMsgBuyName
+	NewMsgSetName    = types.NewMsgSetName
+	NewMsgDeleteName = types.NewMsgDeleteName
+	NewWhois         = types.NewWhois
+	ModuleCdc        = types.ModuleCdc
+	RegisterCodec    = types.RegisterCodec
+)
+
+type (
+	MsgSetName      = types.MsgSetName
+	MsgBuyName      = types.MsgBuyName
+	MsgDeleteName   = types.MsgDeleteName
+	QueryResResolve = types.QueryResResolve
+	QueryResNames   = types.QueryResNames
+	Whois           = types.Whois
+)
